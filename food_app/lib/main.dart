e0416070f6ca733b4d63788c4bf9a7984cbc41c6
yousefmahmou <yousefseko570@gmail.com>
2022-09-13 13:05:57 +0200

    commit

--- food_app/lib/main.dart
+++ food_app/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:food_app/homepage.dart';
+import 'package:food_app/screens/categories_meal_screen.dart';
+import 'package:food_app/screens/categories_screen.dart';
+import 'package:food_app/screens/meal_detail_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,14 +11,49 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
+        //accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(
+          255,
+          254,
+          229,
+          1,
+        ),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(
+                color: Color.fromARGB(
+                  20,
+                  50,
+                  50,
+                  1,
+                ),
+              ),
+              bodyText2: TextStyle(
+                color: Color.fromARGB(
+                  20,
+                  50,
+                  50,
+                  1,
+                ),
+              ),
+              // titleLarge: TextStyle(
+              //   fontSize: 20,
+              //   fontWeight: FontWeight.bold,
+              //   fontFamily: 'RobotoCondensed',
+              // ),
+            ),
       ),
-      home: HomePage(),
+      // home: CategoriesScreen(),
+      routes: {
+        '/': (context) => CategoriesScreen(),
+        CategoriesMealScreen.routeName: (context) => CategoriesMealScreen(),
+        MealDetailScreen.routeName: (context) => MealDetailScreen(),
+      },
     );
   }
 }
---
+++ food_app/lib/screens/categories_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/components/category_item.dart';
+import 'package:food_app/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meal App'),
+      ),
+      body: GridView(
+        padding: EdgeInsets.all(25),
+        children: DUMMY_CATEGORIES
+            .map((catData) => CategoryItem(
+                  catData.id,
+                  catData.title,
+                  catData.color,
+                ))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
